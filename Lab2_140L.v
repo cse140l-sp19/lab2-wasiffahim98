d9aba34f38f6bac764cbf538f665ec559ee31393
Bryan Chin <b5chin@eng.ucsd.edu>
2019-04-17 15:21:48 -0700

    added comment header

--- Lab2_140L.v
+++ Lab2_140L.v
@@ -1,3 +1,22 @@
+// --------------------------------------------------------------------
+// >>>>>>>>>>>>>>>>>>>>>>>>> COPYRIGHT NOTICE <<<<<<<<<<<<<<<<<<<<<<<<<
+// --------------------------------------------------------------------
+// Copyright (c) 2019 by UCSD CSE 140L
+// --------------------------------------------------------------------
+//
+// Permission:
+//
+//   This code for use in UCSD CSE 140L.
+//   It is synthesisable for Lattice iCEstick 40HX.  
+//
+// Disclaimer:
+//
+//   This Verilog source code is intended as a design reference
+//   which illustrates how these types of functions can be implemented.
+//   It is the user's responsibility to verify their design for
+//   consistency and functionality through the use of formal
+//   verification methods.  
+//
 module Lab2_140L (
  input wire Gl_rst,                  // reset signal (active high)
  input wire clk,                     // global clock
