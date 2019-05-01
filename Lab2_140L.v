4aef88fc540c2e952028fb3daefe5ac3d725924a
Wasif Fahim <wasif.fahim@gmail.com>
2019-05-01 12:44:47 -0700

    code is compiling

--- Lab2_140L.v
+++ Lab2_140L.v
@@ -28,6 +28,10 @@ module Lab2_140L (
  output wire L2_adder_rdy          , //pulse
  output wire [7:0] L2_led
 );
+addition add1(Gl_r1, Gl_r2, L2_rdy, L2_adder_data, carryOut);
+//subtract(Gl_r1, Gl_r2, L2_rdy, L2_adder_data);
+sigDelay delay1(L2_adder_rdy, Gl_adder_start, clk, Gl_rst);
+assign L2_led = {carryOut, L2_adder_data};
 
 endmodule
 
@@ -52,3 +56,62 @@ module sigDelay(
    assign sigOut = delayReg[delayVal];
 endmodule // sigDelay
 
+module addition(input[7:0] Gl_r1, input[7:0] Gl_r2, input Gl_subtract, output cout, output[3:0] sum, output carryOut);
+	wire [3:0] a = Gl_r1;
+	wire [3:0] b = Gl_r2;
+	
+	assign a = Gl_r1[3:0];
+	assign b = Gl_r2[3:0];
+	wire cin;
+	assign cin = 1'b0;
+	wire [3:0] c;
+	assign c[0] = b[0] ^ carryOut;
+	assign c[1] = b[1] ^ carryOut;
+	assign c[2] = b[2] ^ carryOut;
+	assign c[3] = b[3] ^ carryOut;
+	
+	singleBit sum0(.a(a[0]), .b(c[0]), .cin(cin), .sum(sum[0]), .cout(bit0));
+	singleBit sum1(.a(a[1]), .b(c[1]), .cin(bit0), .sum(sum[1]), .cout(bit1));
+	singleBit sum2(.a(a[2]), .b(c[2]), .cin(bit1), .sum(sum[2]), .cout(bit2));
+	singleBit sum3(.a(a[3]), .b(c[3]), .cin(bit2), .sum(sum[3]), .cout(carryOut));
+	
+	reg [7:0] d;
+	always @(*)
+	begin
+	if(Gl_subtract == 0) begin
+		if(carryOut == 0) begin
+			d = {4'b0011,c};
+		 end
+		else
+		begin
+			d = {4'b0101,c};
+		end
+	end
+	else
+	begin
+		if(carryOut == 1) begin
+			d = {4'b0011,c};
+		 end
+		else
+		begin
+			d = {4'b0101,c};
+		end
+	end
+	end
+endmodule
+/*
+module subtract(input G1_r1, input Gl_r2, output cout, output sum);
+	reg [3:0] a = Gl_r1;
+	reg [3:0] b = Gl_r2;
+	wire cin;
+	assign cin = 1'b1;
+	singleBit sum0(.a(a[0]), .b(b[0]), .cin(cin), .sum(sum[0]), .cout(bit0));
+	singleBit sum1(.a(a[1]), .b(b[1]), .cin(bit0), .sum(sum[1]), .cout(bit1));
+	singleBit sum2(.a(a[2]), .b(b[2]), .cin(bit1), .sum(sum[2]), .cout(bit2));
+	singleBit sum3(.a(a[3]), .b(b[3]), .cin(bit2), .sum(sum[3]), .cout(bit3));
+endmodule
+*/
+module singleBit(input a, input b, input cin, output sum, output cout);
+	assign sum = a ^ b ^ cin;
+	assign cout = (a & b)|(a & cin)|(b & cin);
+endmodule 
